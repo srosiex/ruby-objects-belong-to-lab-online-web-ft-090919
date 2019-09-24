098ea94f5d1e77109d924c8ba0f36bf6043962b3
Sarah H <sarahrose.henderson@gmail.com>
2019-09-24 14:44:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
-class Song
-  attr_accessor :title, :artist
-  def initialize(title)
-    @title = title
-  end
+class Artist
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+  end
 
 end
